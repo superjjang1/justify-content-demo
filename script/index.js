1c89bbd693d6f90453746c7aa62a78148a6e75be
superjjang1 <51767689+superjjang1@users.noreply.github.com>
2019-07-12 13:09:41 -0400

    added a few more justify stuff

--- script/index.js
+++ script/index.js
@@ -6,3 +6,25 @@ const flexStartDiv = document.querySelector('[data-flex-start-div]');
 flexStart.addEventListener('click', () =>{
     flexStartDiv.classList.toggle('hidden')
 });
+
+const flexEnd = document.querySelector('[data-flex-end]');
+const flexEndDiv= document.querySelector('[data-flex-end-div]');
+
+flexEnd.addEventListener('click', () =>{
+    flexEndDiv.classList.toggle('hidden')
+});
+
+const flexCenter = document.querySelector('[data-flexcenter]');
+const flexCenterBox = document.querySelector('[data-flexCenter-box]')
+
+flexCenter.addEventListener('click', () =>{
+    flexCenterBox.classList.toggle('hidden')
+});
+
+const flexSpaceAround = document.querySelector('[data-flex-space-around]');
+const flexSpaceAroundBox = document.querySelector('[data-flex-space-around-box]')
+
+flexSpaceAround.addEventListener('click', () =>{
+    flexSpaceAroundBox.classList.toggle('hidden')
+});
+
