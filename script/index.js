2928ea5743eaf7a30ae3bf80255163265d1597df
superjjang1 <51767689+superjjang1@users.noreply.github.com>
2019-07-12 10:48:10 -0400

    made the boxes disappear and reappear

--- script/index.js
+++ script/index.js
@@ -1,1 +1,8 @@
 // js
+const flexStart = document.querySelector(' [ data-flex-start ] ');
+const flexStartDiv = document.querySelector('[data-flex-start-div]');
+// magic time
+
+flexStart.addEventListener('click', () =>{
+    flexStartDiv.classList.toggle('hidden')
+});
